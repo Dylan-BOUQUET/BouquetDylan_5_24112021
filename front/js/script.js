fe43788dd774ba9cf1258cb8e64b4a1eca58ce68
Dylan-Bouquet <dylanbouquet66@gmail.com>
2021-11-29 12:58:53 +0100

    Ajout du code dans le fichier script pour l'ffichage des profuits et la connexion à l'api + ajout du code dans le fichier product pour ajouter l'objet les couleurs et autre spécificité

---
+++ front/js/script.js
@@ -0,0 +1,21 @@
+// *** Affiche tout les produits
+let showProduct = (data) => {
+    let items = document.getElementById('items')
+    for (let i in data) {
+        items.innerHTML += `
+    <a href="./product.html?id=${data[i]._id}">
+      <article>
+        <img src="${data[i].imageUrl}" alt="${data[i].altTxt}">
+        <h3 class="productName">${data[i].name}</h3>
+        <p class="productDescription">${data[i].description}</p>
+      </article>
+    </a>`
+    }
+}
+
+// *** Connexion à l'API
+fetch('http://localhost:3000/api/products')
+    .then((response) => response.json())
+    .then((data) => {
+        showProduct(data)
+    })
